ec673825909359fff37af3aee90862309ae420f4
nakshatra1997 <nakshatrapradhan2013@gmail.com>
2016-09-15 00:02:44 +0530

    https://github.com/mps1234/LibraryPortal.git

--- test2.php
+++ test2.php
@@ -1,38 +1,26 @@
 <?php
 require 'PHPMailerAutoload.php';
-$mail = new PHPMailer;
-$email='akshatkumarsrivastava@gmail.com';
- 
-$mail = new PHPMailer;
-//$mail->SMTPDebug = 4;
-$mail->CharSet = 'UTF-8';
-$mail->isSMTP();
-$mail->Debugoutput = 'html';
-$mail->Host = 'smtp.gmail.com';
-$mail->Port = 587;
-$mail->SMTPSecure = 'tls';
-$mail->SMTPAuth = true;
-$mail->Username = 'nakshatrapradhan2013';  
-$mail->Password = '';  //<<--- my gmail password
-$mail->From = 'nakshatrapradhan2013@gmail.com';
-$mail->FromName = 'Title From';
-$mail->isHTML(true);
-$mail->addAddress('akashatsrivastava2@gmail.com');     // Add a recipient
-//$mail->addReplyTo($email, $korpaime.' '.$korpaprezime);
-//$mail->addCC('cc@example.com');
-$mail->addBCC('nakshatrapradhan2013@gmail.com');
-        // Add attachments
-    // Optional name
+require 'class.phpmailer.php';
+require 'class.smtp.php' ;
 
-$mail->Subject = 'first message '.$email;
-$mail->Body = 'mail aaya mail aaya';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail = new PHPMailer(); // create a new object
+$mail->IsSMTP(); // enable SMTP
+$mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+$mail->SMTPAuth = true; // authentication enabled
+$mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+$mail->Host = "smtp.gmail.com";
+$mail->Port = 587; // or 587
+$mail->IsHTML(true);
+$mail->Username = "nakshatrapradhan2013@gmail.com";
+$mail->Password = "Na1@trapra";
+$mail->SetFrom("nakshatrapradhan2013@gmail.com");
+$mail->Subject = "Test";
+$mail->Body = "hello";
+$mail->AddAddress("akshatkumarsrivastava2@gmail.com");
 
-if (!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-    die;
-} else {
-echo 'OK  mail is sended';}
-
-?>
+ if(!$mail->Send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+ } else {
+    echo "Message has been sent";
+ }
+ ?>
